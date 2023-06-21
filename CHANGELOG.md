9c234f70637281e14e22fe421ef23418bdcb5282
Mikhail Fedotov <myf.ivm@gmail.com>
2023-06-21 18:06:40 +0000

    0.0.1.alpha

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog
+
+## [v0.0.1] - 2023-06-20
+
+### Added
+
+- Tap, hold and swipe support.
---
+++ lib/fusuma/plugin/touchscreen/version.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Fusuma
+  module Plugin
+    module Touchscreen
+      VERSION = "0.0.1.alpha"
+    end
+  end
+end
